ff5cbb287a37bf038711a4b26e39003d20e5d095
tscz <tscz@users.noreply.github.com>
2020-02-10 18:48:31 +0100

    #2 Add angular material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jalp-ui app is running!');
+    expect(compiled.querySelector('#content').textContent).toContain('jalp-ui app is running!');
   });
 });
